94abf3f896aeffedd598610fa827138dcba593e4
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 13:34:23 +0100

    Update SmartphonePrice.java

--- src/it/develhope/SmartphonePrice.java
+++ src/it/develhope/SmartphonePrice.java
@@ -1,2 +1,38 @@
-package it.develhope;public class SmartphonePrice {
+package it.develhope;
+
+import java.util.Objects;
+
+public class SmartphonePrice implements Cloneable{
+    public String priceType;
+    public double priceInEuros;
+
+    public SmartphonePrice(String priceType, double priceInEuros){
+        this.priceType = priceType;
+        this.priceInEuros = priceInEuros;
+    }
+
+    @Override
+    public String toString() {
+        return "Price Type = " + priceType +
+                ", price In Euros = " + priceInEuros ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmartphonePrice that = (SmartphonePrice) o;
+        return Double.compare(that.priceInEuros, priceInEuros) == 0 && priceType.equals(that.priceType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(priceType, priceInEuros);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        SmartphonePrice clonedSmartphonePrice = (SmartphonePrice) super.clone();
+        return clonedSmartphonePrice;
+    }
 }
